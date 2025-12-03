5a517156b5a7e469e1370fe7a9a02fb5de05e38c
HP189-pr <ksvtsdoc@gmail.com>
2025-12-03 15:11:40 +0530

    all

--- src/config/adminMenuConfig.js
+++
@@ -1,9 +0,0 @@
-const adminMenuConfig = {
-    "User Management": ["User List", "Add User", "User Rights"],
-    "Role Management": ["Role Permissions", "Department Rights"],
-    "College Management": ["Add New College", "Manage Colleges"],
-    "Course Management": ["Add New Course", "Manage Courses"]
-  };
-  
-  export default adminMenuConfig;
-  
